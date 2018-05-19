6ec03557d503bf732d72728887f1d8b4109be1e3
Jonatan Schlag <jonatan.schlag@ipfire.org>
2018-05-19 10:48:52 +0200

    Improving error message and undefining of network objects
    Signed-off-by: Jonatan Schlag <jonatan.schlag@ipfire.org>

--- src/nitsi/network.py
+++ src/nitsi/network.py
@@ -10,20 +10,31 @@ class network():
     def __init__(self, libvirt_con, network_xml_file):
         self.log = logger.getChild(os.path.basename(os.path.dirname(network_xml_file)))
         self.con = libvirt_con
+        self.network = None
         try:
             with open(network_xml_file) as fobj:
+                self.log.info("Reading xml file for network")
                 self.network_xml = fobj.read()
         except FileNotFoundError as error:
             self.log.error("No such file: {}".format(network_xml_file))
+            raise error
 
     def define(self):
+        self.log.info("Going to define network")
         self.network = self.con.networkDefineXML(self.network_xml)
 
         if network == None:
             self.log.error("Failed to define virtual network")
 
     def start(self):
+        self.log.info("Starting Network")
         self.network.create()
 
     def undefine(self):
-        self.network.destroy()
+        if self.network != None:
+            if self.network.isActive() == 1:
+                self.log.info("Shutting down network")
+                self.network.destroy()
+
+            self.log.info("Undefining network")
+            self.network.undefine()
